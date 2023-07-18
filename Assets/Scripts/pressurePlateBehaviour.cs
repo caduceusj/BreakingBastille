5c9e996a0ed5e95788e234fbb190222b6b252535
marcos-arthur <marcos.marcos.arthur291@gmail.com>
2023-07-18 19:00:07 -0300

    Refatoração do level 1 + bake do lightmap

--- Assets/Scripts/pressurePlateBehaviour.cs
+++ Assets/Scripts/pressurePlateBehaviour.cs
@@ -5,70 +5,35 @@ using UnityEngine;
 public class pressurePlateBehaviour : MonoBehaviour
 {
     // Start is called before the first frame update
-    private Vector3 originalPos;
+    [SerializeField] private List<GameObject> trapsAssociated;
+    [SerializeField] private GameObject door;
+
     private LevelController levelController;
-    private bool trigger,block,moveBack;
-    [SerializeField]private List<GameObject> trapsAssociated;
     
+    private bool block;
+
     void Start()
     {
         levelController = LevelController.Instance;
         block = false;
-        originalPos = transform.position;
     }
 
     private void OnCollisionEnter(Collision other)
     {
-        //Debug.Log("Colisão Detectada");
         if (other.gameObject.CompareTag("Player") && block == false) {
-            transform.parent = transform;
-            trigger = true;
-        }
-    }
-    /*private void OnCollisionStay(Collision other)
-    {
-        if (other.gameObject.CompareTag("Player") && block == false)
-        {
-            transform.position -= new Vector3(0, 0.01f, 0);
-            moveBack = false;
-        }
-    }
-    private void OnCollisionExit(Collision other)
-    {
-        if (other.gameObject.CompareTag("Player") && block == false)
-        {
-            moveBack = true;
-            other.transform.parent = null;
-        }
-    }*/
 
-    // Update is called once per frame
-    void Update()
-    {
-        /*if (moveBack)
-        {
-            if(transform.position.y < originalPos.y)
-            {
-                transform.position += new Vector3(0, 0.01f, 0);
-            }
-            else
-            {
-                moveBack = false;
-            }
-        }*/
-        if(trigger)
-        {
             if (levelController.checkStatues())
             {
-                block = true;               
+                block = true;
+                door.GetComponent<Animator>().SetBool("isOpen", true);
             }
             else
             {
-                foreach (GameObject trap in trapsAssociated) {
-                    trap.transform.GetChild(0).GetComponent<TrapBehaviour>().shoot();                        
+                foreach (GameObject trap in trapsAssociated)
+                {
+                    trap.GetComponent<TrapBehaviour>().shoot();
                 }
             }
-            trigger = false;
         }
     }
 }
