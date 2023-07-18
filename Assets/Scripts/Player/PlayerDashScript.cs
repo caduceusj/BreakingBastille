d7bcf1344ef5fba38e26c2055004202feae1a0da
João Anisio <joaoanisiomn@hotmail.com>
2023-07-18 13:58:34 -0300

    AJEITA A BARRA MARCOS

--- Assets/Scripts/Player/PlayerDashScript.cs
+++ Assets/Scripts/Player/PlayerDashScript.cs
@@ -2,23 +2,47 @@ using StarterAssets;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class PlayerDashScript : MonoBehaviour
 {
     
     public float dashTime ;
     public float dashSpeed;
+    public float currentCooldown = 0f;
+
+    [SerializeField] Image cooldownBar; // Reference to the UI bar image
+    [SerializeField] float fillSpeed = 2f; // Speed at which the UI bar fills
+
+    public float maxCooldown = 5f; // Set your desired cooldown time here
     public FirstPersonController firstPersonController;
+    public PlayerStaminaBarController playerStaminaBarController;
     // Start is called before the first frame update
     void Start()
     {
+
         firstPersonController = GetComponent<FirstPersonController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Q)) {
+
+        if (currentCooldown > 0f)
+        {
+            currentCooldown -= Time.deltaTime;
+
+            // Ensure the cooldown doesn't go below zero
+            currentCooldown = Mathf.Max(currentCooldown, 0f);
+        }
+
+
+        playerStaminaBarController.UpdateCooldownUI();
+
+        
+        // Update the UI bar to reflect the cooldown progress
+
+
+        if (Input.GetKeyDown(KeyCode.Q)) {
             StartCoroutine(Dash());
         }
 
@@ -27,13 +51,19 @@ public class PlayerDashScript : MonoBehaviour
 
     IEnumerator Dash()
     {
-        float startTime = Time.time;
-        while (Time.time < startTime + dashTime)
+
+        if (currentCooldown <= 0f)
         {
+            float startTime = Time.time;
+            while (Time.time < startTime + dashTime)
+            {
 
-            firstPersonController._controller.Move((firstPersonController.transform.right * firstPersonController._input.move.x + firstPersonController.transform.forward * firstPersonController._input.move.y ).normalized*  dashSpeed *  Time.deltaTime);
-            yield return null;
+                firstPersonController._controller.Move((firstPersonController.transform.right * firstPersonController._input.move.x + firstPersonController.transform.forward * firstPersonController._input.move.y).normalized * dashSpeed * Time.deltaTime);
+                yield return null;
+                currentCooldown = maxCooldown;
+
+            }
 
         }
-        }
+    }
 }
---
+++ Assets/Scripts/Player/PlayerStaminaBarController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerStaminaBarController : MonoBehaviour
+{
+    PlayerDashScript playerDashScript;
+
+    public GameObject player;
+
+    [SerializeField] Image cooldownBar; // Reference to the UI bar image
+    [SerializeField] float fillSpeed = 0.5f; // Speed at which the UI bar fills
+    
+    public void Start()
+    {
+        playerDashScript = player.GetComponent<PlayerDashScript>();
+    }
+    public void UpdateCooldownUI()
+    {
+
+        float cooldownProgress = playerDashScript.currentCooldown / playerDashScript.maxCooldown;
+
+        Debug.Log(playerDashScript.currentCooldown);
+        Debug.Log(playerDashScript.maxCooldown);
+
+        float targetFillAmount = cooldownProgress;
+
+        // Smoothly interpolate the fill amount using Lerp
+        cooldownBar.fillAmount = Mathf.Lerp(cooldownBar.fillAmount, targetFillAmount, Time.deltaTime * fillSpeed);
+
+    }
+
+
+    void Update()
+    {
+        UpdateCooldownUI();
+    }
+}
